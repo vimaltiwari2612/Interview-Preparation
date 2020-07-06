cf27eef4d1bdefb86e7906132065ea10d297a074
Vimal Tiwari <vimaltiwari2612@gmail.com>
2020-07-06 17:30:18 +0530

    Add files via upload

--- GS coding questions/Shortest path Algo from source to nodes.java
+++ GS coding questions/Shortest path Algo from source to nodes.java
@@ -6,6 +6,9 @@ public class ShortestPath{
 		The Floyd Warshall Algorithm is for solving the All Pairs Shortest Path problem. 
 		The problem is to find shortest distances between every pair 
 		of vertices in a given edge weighted directed Graph.
+		 O(V3)
+		 
+		 /// - It can't work with negative weights
 	*/
 	static int[][] FloydWarshall(int[][] dist){
 		/* Add all vertices one by one to the set of intermediate 
@@ -37,4 +40,64 @@ public class ShortestPath{
 	}
 	
 	
+		// The main function that finds shortest 
+	// distances from src to all other vertices 
+	// using Bellman-Ford algorithm. The function 
+	// also detects negative weight cycle 
+	// The row graph[i] represents i-th edge with 
+	// three values u, v and w. 
+	/*
+		input - graph [srcVertex,DesinationVertes,weight]
+		/// - It can detect with negative weights cycles
+	*/
+	static int[] BellmanFord(int graph[][], int V, int E, 
+					int src) 
+	{ 
+		// Initialize distance of all vertices as infinite. 
+		int [] dis = new int[V]; 
+		for (int i = 0; i < V; i++) 
+			dis[i] = Integer.MAX_VALUE; 
+	  
+		// initialize distance of source as 0 
+		dis[src] = 0; 
+	  
+		// Relax all edges |V| - 1 times. A simple 
+		// shortest path from src to any other 
+		// vertex can have at-most |V| - 1 edges 
+		for (int i = 0; i < V - 1; i++)  
+		{ 
+	  
+			for (int j = 0; j < E; j++)  
+			{ 
+				if (dis[graph[j][0]] + graph[j][2] < 
+								dis[graph[j][1]]) 
+					dis[graph[j][1]] =  
+					dis[graph[j][0]] + graph[j][2]; 
+			} 
+		} 
+	  
+		// check for negative-weight cycles. 
+		// The above step guarantees shortest 
+		// distances if graph doesn't contain 
+		// negative weight cycle. If we get a 
+		// shorter path, then there is a cycle. 
+		for (int i = 0; i < E; i++)  
+		{ 
+			int x = graph[i][0]; 
+			int y = graph[i][1]; 
+			int weight = graph[i][2]; 
+			if (dis[x] != Integer.MAX_VALUE && 
+					dis[x] + weight < dis[y]) 
+				System.out.println("Graph contains negative"
+						+" weight cycle"); 
+		} 
+	  
+		System.out.println("Vertex Distance from Source"); 
+		for (int i = 0; i < V; i++) 
+			System.out.println(i + "\t\t" + dis[i]); 
+		
+		return dis;
+	} 
+	
+			
 }
--- LeetCode/GraphTraversal.java
+++ LeetCode/GraphTraversal.java
@@ -29,17 +29,17 @@ class Traversal {
 	//use Stack instead of queue
 	static ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> g, int N)
     {
-        // add your code here
-        ArrayList<Integer> visited = new ArrayList<Integer>();
+       // add your code here
+        
         Stack<Integer> stack = new Stack<Integer>();
         ArrayList<Integer> fvisited = new ArrayList<Integer>();
         
         int source = 0;//the first node
         stack.push(source);
-        visited.add(source);
+        
         
         while(!stack.isEmpty()){
-            System.out.println(stack);
+          
             int s = stack.peek();
             stack.pop();
             fvisited.add(s);
@@ -47,8 +47,8 @@ class Traversal {
             ArrayList<Integer> nodes = g.get(s);
             for(int node : nodes){
                 //if not visited
-                if(!visited.contains(node)){
-                    visited.add(node);
+                if(!fvisited.contains(node)){
+                    
                     stack.push(node);
                 }
             }
